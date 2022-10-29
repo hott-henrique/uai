9b5056f7541c6f8cf0fab7a68f91cd11c68aad3c
Henrique Hott <henriquehott@pm.me>
2022-10-29 14:34:37 -0300

    Solution: R0.19

---
+++ Roteiros/R0/obihanoi.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    int N = 1;
+
+    int counter = 1;
+
+    while (1) {
+        cin >> N;
+
+        if (!N) break;
+
+        int o = N, d = 0, t = 0;
+
+        int answ = 0;
+
+        cout << "Teste " << counter << '\n';
+        cout << (unsigned long long)(pow(2, N)) - 1 << "\n\n";
+
+        counter++;
+    }
+
+    return 0;
+}
