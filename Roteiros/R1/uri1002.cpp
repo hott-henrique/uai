d33a0c9eafc62ae626418fb20476c0730f6ea034
Henrique Hott <henriquehott@pm.me>
2022-10-29 21:20:47 -0300

    R1.3

---
+++ Roteiros/R1/uri1002.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    double r;
+    cin >> r;
+
+    cout << fixed << showpoint;
+    cout << setprecision(4);
+    cout << "A=" << r * r * 3.14159  << '\n';
+
+    return 0;
+}
